b4d8b8b2716c6c7e84420b81914d57e190b73fa5
ahmetcerol <erola2917@gmail.com>
2023-10-16 12:24:21 +0300

    Reverse Integer

---
+++ ReverseInteger.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        
+        long reversedx=0;
+        int remainder=0;
+
+        int temp = x;
+
+        while (temp != 0) {
+            remainder = temp % 10;
+            reversedx = (reversedx * 10) + remainder;
+            temp /= 10;
+        }
+        if(reaversedx > Integer.MAX_VALUE || reversedx < Integer.MIN_VALUE)
+            return 0;
+        return (int)reversedx;
+    }
+}
